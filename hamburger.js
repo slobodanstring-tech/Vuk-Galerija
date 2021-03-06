1414fe546beccdabddfb4d18addb65a75b222b6a
StringTech <slobodanboba23@gmail.com>
2021-03-06 12:52:13 +0100

    Dorada1

--- hamburger.js
+++ hamburger.js
@@ -1,3 +1,19 @@
+const hamburgerOpen = document.querySelector(".hamburger");
+const linkovi = document.querySelector("#ul")
+const headerRelative = document.querySelector("#header")
+const sviLinkovi = document.querySelectorAll("#ul.a")//Biras ovako sve linkove koji se nalaze u ulicama
+
+
+hamburgerOpen.addEventListener("click",()=>{
+linkovi.classList.toggle("mobil-ul")
+})
+hamburgerOpen.addEventListener("click",()=>{
+headerRelative.classList.toggle("header-relative")
+})
+hamburgerOpen.addEventListener("click",()=>{
+  sviLinkovi.classList.toggle("header-relative")
+})
+
 
 
 
